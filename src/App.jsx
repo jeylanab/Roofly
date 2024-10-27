90f4a1f6a251086b21a03a33551af31d468580c1
jeylanab <jeylantesi@gmail.com>
2024-10-27 10:38:22 +0530

    added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { React } from "react"
+import { Home } from "./component/Home"
+import { Navigation } from "./component/Navigation"
+import { Footer } from "./component/Footer"
+import { Login } from "./component/Login"
+import { SignUp } from "./component/SignUp"
+import { About } from "./component/About"
+
+
+
 
 function App() {
 
   return (
     <div>
-      Hello World
+      <Navigation/>
+      <Home/>
+      
     </div>
   )
 }
