cd048d4b87456dd5babc5334ef90bf52f0393af6
jeylanab <jeylantesi@gmail.com>
2024-10-27 12:38:23 +0530

    sign up button

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -6,17 +6,21 @@ export const Navigation = () => {
   return (
     <div>
       {/* display navigation menus only for large device  */}
-      <div className='menu-container-large  lg:flex hidden items-center justify-between'>
+      <div className='menu-container-large  lg:flex hidden items-center justify-between mx-10'>
         <div className='logo-container flex items-center'>
           {/* display the logo of the company */}
            <img src={logo} alt="roofly logo" />
         </div>
         {/* map through the menulist object to display the menu items */}
-        <ul className='list-none flex justify-center items-center'>
+        <ul className='list-none flex justify-center items-center text-lg'>
           {menuList.map((menu) => {
          return <li className='mx-5' key={menu.id}>{ menu.title }</li>
           })}
         </ul>
+        <div className='flex justify-center items-center text-lg'>
+          <p className='mx-3'>Sign In</p>
+          <button className='btn-one px-5 py-1 text-lg'>Sign Up</button>
+        </div>
         
       </div>
     </div>
