9302e1a62cc97d02c3055e2e8226882d28cfa384
jeylanab <jeylantesi@gmail.com>
2024-10-27 14:44:33 +0530

    search menu added

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import home from '../asset/home.mp4';
 import searchIcon from "../asset/search-icon.svg"
+import check from "../asset/check.svg"
 
 export const Home = () => {
   return (
@@ -14,9 +15,9 @@ export const Home = () => {
         <source src={home} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="absolute inset-0 bg-slate-300 opacity-70"></div> {/* Dark overlay */}
+      <div className="absolute inset-0 bg-slate-300 opacity-60"></div> {/* Dark overlay */}
       <div className='relative z-10 flex items-start justify-around my-20 h-full'>
-        <div>
+        <div >
           <h1 className='text-4xl font-bold'>Find the Perfect Property</h1>
           <p className='py-5 text-xl'>From cozy apartments to luxurious estates,<br /> we have something for everyone.</p>
           <div className='flex items-center my-10'>
@@ -25,14 +26,31 @@ export const Home = () => {
               <img src={searchIcon} alt="search icon" />
             </div>
           </div>
+          <div className='flex  py-2'>
+              <div className='flex '>
+                 <img src={check} alt="" />
+                 <p>100% Guarantee</p>
+               </div>
+               <div className='flex px-3 '>
+                <img src={check} alt="check icon" />
+                <p>Expert Guidance</p>
+               </div>
+          </div>
+            <div className='flex py-2 '>
+              <div className='flex '>
+                 <img src={check} alt="" />
+                 <p>Client Satisfaction</p>
+               </div>
+               <div className='flex px-3 '>
+                <img src={check} alt="check icon" />
+                <p>Exclusive Listings</p>
+               </div>
+          </div>
 
         </div>
-        <div>
-        </div>
-        <div>
-          
-        </div>
+        <div>dfaljsk</div>
       </div>
+      
     </div>
   );
 }
